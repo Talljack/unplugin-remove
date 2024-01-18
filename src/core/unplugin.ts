4413a7bc7793441adf7abbd7840640108943c061
Yugang Cao <1442770751@qq.com>
2024-01-18 11:46:28 +0800

    feat: update deps (#9)
    * feat: deps

--- src/core/unplugin.ts
+++ src/core/unplugin.ts
@@ -2,6 +2,7 @@ import { createUnplugin } from 'unplugin'
 import type { Options } from '../types'
 import { defaultOptions } from './constant'
 import { createUnpluginContext } from './unpluginContext'
+
 export default createUnplugin<Options>(options => {
   const ctx = createUnpluginContext(options ?? defaultOptions)
   return {
