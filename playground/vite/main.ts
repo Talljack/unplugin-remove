6e2210db8ef23f49363bc82ab803215eb1ee13fb
Yugang Cao <1442770751@qq.com>
2024-01-18 22:23:27 +0800

    fix: logic remove (#17)

--- playground/vite/main.ts
+++ playground/vite/main.ts
@@ -8,6 +8,20 @@ console.log(
 )
 document.getElementById('app')!.innerHTML = `__UNPLUGIN__${count}`
 
+console.log('00000')
+const a = true;
+
+!a && console.log('1111111')
+
+a || console.log(2222)
+
+a ?  2 : console.log('010101')
+
+function test0() {
+  const b = a && console.log(111)
+  console.log(b)
+}
+
 console.log('hello world')
 
 const test = () => console.log(123)
