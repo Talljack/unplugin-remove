9e15804b1964c9e9249b657a24d635dd7793f65c
Yugang Cao <yugang.cao12@gmail.com>
2024-05-03 22:07:34 +0800

    workflow: eslint

--- src/types.ts
+++ src/types.ts
@@ -3,27 +3,23 @@ import type { FilterPattern } from '@rollup/pluginutils'
 export interface Options {
   /**
    * don't remove console.log and debugger these module
-   *
    * @default []
    */
   external?: Array<string>
 
   /**
    * remove console type of these module
-   *
    * @default ['log']
    */
   consoleType?: Array<'log' | 'warn' | 'error' | 'info' | 'debug'>
   /**
    * Rules to include transforming target.
-   *
    * @default [/\.[jt]sx?$/, /\.vue\??/]
    */
   include?: FilterPattern
 
   /**
    * Rules to exclude transforming target.
-   *
    * @default [/node_modules/, /\.git/]
    */
   exclude?: FilterPattern
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,11 +6,17 @@ import t from '@babel/types'
 import type { Options } from '../types'
 
 export const getAbsoluteFilePaths = (filePaths: string[]) => {
-  return filePaths.map(filePath => {
+  return filePaths.map((filePath) => {
     return path.isAbsolute(filePath) ? filePath : path.resolve(process.cwd(), filePath)
   })
 }
 
+/**
+ *
+ * @param code
+ * @param types
+ * @param sourceMaps
+ */
 export function removeConsoleLogs(code: string, types: Options['consoleType'], sourceMaps = false) {
   const ast = parser.parse(code, {
     sourceType: 'unambiguous',
@@ -37,6 +43,10 @@ export function removeConsoleLogs(code: string, types: Options['consoleType'], s
   })
 }
 
+/**
+ *
+ * @param code
+ */
 export function removeDebugger(code: string) {
   return code.replace(/debugger;?/g, '')
 }
