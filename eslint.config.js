1c9961f337991690ccb89204be59af6bfb788eb0
Yugang Cao <1442770751@qq.com>
2024-05-03 22:12:14 +0800

    workflow: eslint (#183)
    * workflow: eslint
    
    * feat: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
-import antfu from '@antfu/eslint-config'
+// @ts-check
+import createEslintConfig from 'talljack-eslint-config'
 
-export default antfu({
+export default createEslintConfig({
   typescript: true,
+  formatters: true,
   rules: {
     'antfu/top-level-function': 'off',
     'style/arrow-parens': 'off',
     'node/prefer-global/process': 'off',
     'no-debugger': 'off',
     'no-console': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
 })
